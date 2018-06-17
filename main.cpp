8927f27aa80c51a987b591ebffb8fdd5ea487eac
Leigh Garbs <leighgarbs@gmail.com>
2018-06-17 18:19:05 -0400

    New signal handling added to main, SleepProxy class itself still unmodified

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include "SleepProxy.hpp"
 
+SleepProxy* sproxyp = 0;
+
+extern "C" void handle_signal(int sig)
+{
+    if (sproxyp)
+    {
+        sproxyp->signal(sig);
+    }
+}
+
 int main(int argc, char** argv)
 {
-    SleepProxy sproxy(argc, argv, 1);
+    // Need to update rate
+    SleepProxy sproxy(argc, argv, 0.1);
+    sproxyp = &sproxy;
 
     sproxy.run();
 
