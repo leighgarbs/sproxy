fc030aa0c72b08c95fb3344419dd8a5ed7f6b40a
Leigh Garbs <leighgarbs@gmail.com>
2018-08-22 21:42:35 -0400

    WIP

--- SleepProxy.cpp
+++ SleepProxy.cpp
@@ -294,51 +294,6 @@ void SleepProxy::shutdown()
 }
 
 //==============================================================================
-// Obtains the MAC and IP address of the interface to be used
-//==============================================================================
-void SleepProxy::obtain_own_mac_and_ip()
-{
-    // Getting MAC and IP addresses requires a socket, doesn't matter what kind
-    int sock_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
-    if (sock_fd == -1)
-    {
-        // If something goes wrong, print an error message and quit
-        perror(0);
-        shutdown();
-    }
-
-    // Fill out an ifreq with name of the target interface
-    ifreq iface;
-    strcpy(iface.ifr_name, interface_name.c_str());
-
-    // Request our MAC address
-    if (ioctl(sock_fd, SIOCGIFHWADDR, &iface) == -1)
-    {
-        // If something goes wrong, print an error message and quit
-        perror(0);
-        shutdown();
-    }
-
-    // Initialize own_mac
-    memcpy(own_mac, iface.ifr_hwaddr.sa_data, 6);
-
-    // Request our IP address
-    if (ioctl(sock_fd, SIOCGIFADDR, &iface) == -1)
-    {
-        // If something goes wrong, print an error message and quit
-        perror(0);
-        shutdown();
-    }
-
-    // Initialize own IP address
-    sockaddr_in* temp_addr = (sockaddr_in*)&iface.ifr_addr;
-    memcpy(own_ip, (const void*)(&(temp_addr->sin_addr.s_addr)), 4);
-
-    // Not needed anymore; return code not useful enough to bother with
-    close(sock_fd);
-}
-
-//==============================================================================
 // Issuing a WOL
 //==============================================================================
 void SleepProxy::log_issuing_wol(const unsigned char* const mac_address,
