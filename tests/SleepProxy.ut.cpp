bf230d2802bfd28619b907447437a9344e3c1d77
Leigh Garbs <leighgarbs@gmail.com>
2018-08-28 20:48:36 -0400

    SleepProxy test is now skipped when run in the wrong environment

--- tests/SleepProxy.ut.cpp
+++ tests/SleepProxy.ut.cpp
@@ -1,7 +1,20 @@
+#include <stdexcept>
+
 #include "SleepProxy.hpp"
 
 int main(int argc, char** argv)
 {
-    SleepProxy sleep_proxy(argc, argv, 0.1);
-    return 0;
+    int return_code = 0;
+
+    try
+    {
+        SleepProxy sleep_proxy(argc, argv, 0.1);
+    }
+    catch (std::runtime_error ex)
+    {
+        // Return the "skipped" code
+        return_code = 2;
+    }
+
+    return return_code;
 }
